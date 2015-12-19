5765b07e675ddf72a8dbe516c93dd0c691286fce
Peder Bergebakken Sundt <pbsds@hotmail.com>
2015-12-19 22:02:29 +0100

    Support for channels and basic percussion

--- audio_renderer.py
+++ audio_renderer.py
@@ -1,7 +1,7 @@
-import pyaudio, math, itertools, struct
+import pyaudio, math, itertools, struct, random
 
 #global settings
-CHUNK = 256
+CHUNK = 512
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
@@ -10,81 +10,14 @@ RECORD_SECONDS = 5
 #internal globals
 renders = 0
 
-class generatorBase():
-	frequency = 44100.
-	channels = 2
-	velocity_max = 128.#the scale/coeficient
-	def __init__(self):
-		self.notes = []#[i] = [note id, velocity]
-		self.pos = 0#position
-	
-	#input
-	def set_note(self, id, velocity):
-		if id not in map(self._map_i0, self.notes):
-			self.notes.append([id, float(velocity)/(self.velocity_max*3)])
-		else:
-			for i, note in enumerate(self.notes):
-				if note[0] == id:
-					self.notes[i][1] = float(velocity)/(self.velocity_max*3)
-					break
-	def stop_note(self, id):
-		for i, e in enumerate(self.notes):
-				if e[0] == id:
-					del self.notes[i]
-					break
-	
-	def get_frames(self, chunk):#render frames
-		out = []
-		
-		#speedup the name lookup a little
-		chn = self.channels
-		f = self.frequency
-		notes = tuple(self.notes)
-		
-		if len(notes):
-			for i in map(float, xrange(self.pos, self.pos+chunk)):# / self.channels):
-				period = i/f
-				frame = sum(self.tone_generator(period * self.get_freq(n)) * v for n, v in notes)
-				for _ in xrange(chn):
-					out.append(frame if -1 <= frame <= 1 else frame/abs(frame))
-					#out.append(frame)
-		else:
-			out = [0.]*chunk*chn
-		self.pos += chunk
-		
-		return self.pack(out)
-	
-	#To be replaced by children:
-	def tone_generator(self, p):#a period is from 0.0 to 1.0. p is not run trough modulo 1 first
-		return 0#placeholder
-	def get_freq(self, note):#tune for c4 = 60 and A = 440Hz
-		return 440.*math.pow(2, float(note-60)/12.)
-	
-	#internal:
-	def _map_i0(self, i):
-		return i[0]
-	#todo: determine the fastest of the two: (maybe even implement it in numpy instead?)
-	def pack(self, frames):#this handles bit-depth aswell. "frame" is a float between -1 and 1
-		h = "h"
-		p = struct.pack
-		return "".join((p(h, int(i*0x7FFF)) for i in frames))
-	def pack2(self, frames):
-		l = len(frames)
-		scaled = map(mul, frames, [0x7FFF]*l)
-		inted = map(int, scaled)
-		return struct.pack("h"*len(frames), *inted)
-
-class sineGenerator(generatorBase):
-	def tone_generator(self, p):
+#waves:
+def sineWave(p):
 		return math.sin(p*math.pi*2)#slow, names, ech. it hurts
-class squareGenerator(generatorBase):
-	def tone_generator(self, p):
+def squareWave(p):
 		return 1. if (p%1) > 0.5 else -1.
-class sawtoothGenerator(generatorBase):
-	def tone_generator(self, p):
+def sawtoothWave(p):
 		return (p%1)*2. - 1.
-class triangleGenerator(generatorBase):
-	def tone_generator(self, p):
+def triangleWave(p):
 		p = (p%1)*4
 		if p <= 1:
 			return p
@@ -92,8 +25,7 @@ class triangleGenerator(generatorBase):
 			return p-4
 		else:
 			return 2-p
-class dafuqGenerator(generatorBase):#a happy little accident, oc plz donut iron
-	def tone_generator(self, p):
+def dafuqWave(p):#a happy little accident
 		p = (p%1)*4
 		if p <= 1:
 			return p
@@ -101,6 +33,96 @@ class dafuqGenerator(generatorBase):#a happy little accident, oc plz donut iron
 			return p-2
 		else:
 			return 1-p
+#modifiers:
+def AddAttack2Wave(wave, length=1.0):#length in kinda seconds untill the velocity is halved
+	l = 440.0 * length
+	def NewWave(p):
+		return wave(p)/(1. + p/l)
+	return NewWave
+def AddCrush2Wave(wave, levels=100):#length in kinda seconds untill the velocity is halved
+	def NewWave(p):
+		return float(int(wave(p)*levels))/levels
+	return NewWave
+#perc
+def highhatBeat(p):#short percussion
+	#return (random.random()*2-1)/(1+p/10)
+	return (random.random()*2-1) * max(1-p/30, 0)
+def snareBeat(p):
+	return min(max((random.random()*2-1) * max(2-p/24, 0), -1), 1)
+def drumBeat(p):
+	return triangleWave(math.sqrt(1.8*p))*1.2
+
+	
+#generator
+class generator():
+	frequency = float(RATE)
+	channels = 2
+	
+	instruments = [sineWave for _ in xrange(16)]#15 channels instead?
+	
+	def __init__(self):
+		self.notes = [set() for _ in xrange(16)]
+		self.note = []#[i] = [channel, note, velocity, start pos, instrument, frequency]
+		self.pos = 0#position
+	
+	#input
+	def set_note(self, channel, note, velocity, modify=False):
+		if note not in self.notes[channel]:
+			self.note.append([channel, note, float(velocity)/3, self.pos, self.instruments[channel], self.get_freq(note)])
+			self.notes[channel].add(note)
+		else:
+			for i, n in enumerate(self.note):
+				if n[0] == channel and n[1] == note:
+					self.note[i][2] = float(velocity)/3
+					if not modify: self.note[i][3] = self.pos
+					return
+	def stop_note(self, channel, note):
+		for i, n in enumerate(self.note):
+			if n[0] == channel and n[1] == note:
+				del self.note[i]
+				self.notes[channel].remove(note)
+				return
+	
+	def get_frames(self, chunk):#render frames
+		out = []
+		
+		#speedup the name lookup a little
+		chn = self.channels
+		f = self.frequency
+		notes = tuple(self.note)
+		
+		if len(notes):
+			for i in map(float, xrange(self.pos, self.pos+chunk)):
+				frame = sum(w((i-p)/f * freq) * v for c, n, v, p, w, freq in notes)
+				
+				if -1 <= frame <= 1:
+					for _ in xrange(chn):
+						out.append(frame)
+				else:
+					for _ in xrange(chn):
+						out.append(math.copysign(1, frame))
+					#out.append(frame)
+		else:
+			out = [0.]*chunk*chn
+		self.pos += chunk
+		
+		return self.pack(out)
+	
+	#change this for other tunings:
+	def get_freq(self, note):#tune for c4 = 60 and A = 440Hz
+		return 440.*math.pow(2, float(note-58)/12.)
+	
+	#internal:
+	#todo: determine the fastest of the two: (maybe even implement it in numpy instead?)
+	def pack(self, frames):#this handles bit-depth aswell. "frame" is a float between -1 and 1
+		p = struct.pack
+		n = int
+		return "".join((p("h", n(i*0x7FFF)) for i in frames))
+	def pack2(self, frames):
+		l = len(frames)
+		scaled = map(mul, frames, [0x7FFF]*l)
+		inted = map(int, scaled)
+		return struct.pack("h"*l, *inted)
 
 			
 def play(generator):
--- keyboard.py
+++ keyboard.py
@@ -5,23 +5,22 @@ mido.set_backend('mido.backends.pygame')#change this if you use something else
 import audio_renderer as audio
 
 
-def main(generatorClass, keyboard=None, midifile=None):
+def main(gen, keyboard=None, midifile=None, silent=False):
 	if keyboard:
 		midi_stream = mido.open_input(keyboard)
 	elif midifile:
 		midi_stream =  mido.MidiFile(midifile).play()
 	else:
-		return
+		raise Exception("wrong input dumb dumb")
 	
-	gen = generatorClass()
 	stream = audio.play(gen)
 	
-	
+	debug = not silent
 	prev_clock = -0
 	last_few_renders = []
 	renderRate = str(round(float(audio.RATE)/audio.CHUNK, 2))
 	for i in midi_stream:
-		if i.type == "clock" or time.time()-prev_clock > 1./25.:#debug data
+		if i.type == "clock" or time.time()-prev_clock > 1./30. and debug:#debug data
 			t = time.time()
 			h = t-prev_clock
 			prev_clock = t
@@ -36,31 +35,36 @@ def main(generatorClass, keyboard=None, midifile=None):
 			
 			r = round(sum(map(lambda x:x[0], last_few_renders)) / sum(map(lambda x:x[1], last_few_renders)), 2)
 			
-			print "\nMido clock @%sHz, Render @%s/%sHz, Notes: %i" % (str(h).zfill(5), str(r).zfill(len(renderRate)), renderRate, len(gen.notes)),
+			print "\nMido clock @%sHz, Render @%s/%sHz, Notes: %i," % (str(h).zfill(5), str(r).zfill(len(renderRate)), renderRate, len(gen.note)),
 		
 		if i.type == "note_on":#note input
 			if i.velocity == 0:
-				gen.stop_note(i.note)
+				gen.stop_note(i.channel, i.note)#todo: separate with channels
 			else:
-				gen.set_note(i.note, i.velocity)
-			print "note(%i,%i)" % (i.note, i.velocity),
+				gen.set_note(i.channel, i.note, float(i.velocity)/127.)
+			if debug: print "note(%i,%i)" % (i.note, i.velocity),
 		elif i.type == "note_off":
-			gen.stop_note(i.note)
-		elif i.type not in ("clock",):
+			gen.stop_note(i.channel, i.note)
+		elif i.type not in ("clock",) and debug:
 			print i.type,
-			#if i.type == "control_change": print dir(i)
+			if i.type == "control_change": 
+				#print dir(i)
+				print i.control, 
+			if i.type == "program_change": 
+				#print dir(i)
+				print i.program, #changes i.channel to the instrument here. https://en.wikipedia.org/wiki/General_MIDI#Program_change_events
 	
 	midi_stream.close()
 
 if __name__ == "__main__":
-	
-	f, k = None, None
+	f, k, s	= None, None, False
 	#k = mido.get_input_names()[0]#todo: give the user a choice?
 	
 	#f = "midis/13417_Ballad-of-the-Windfish.mid"
 	#f = "midis/27641_Green-Greens.mid"
 	#f = "midis/Clock Town 2.mid"
 	#f = "midis/Clock Town.mid"
+	f = "midis/Darude_-_Sandstorm.mid"
 	#f = "midis/file.mid"
 	#f = "midis/gerudo valley.mid"
 	#f = "midis/Good Egg Galaxy.mid"
@@ -68,7 +72,7 @@ if __name__ == "__main__":
 	#f = "midis/Hare Hare Yukai.mid"
 	#f = "midis/he is a pirate.mid"
 	#f = "midis/kdikarus.mid"
-	f = "midis/Makrells.mid"
+	#f = "midis/Makrells.mid"
 	#f = "midis/mt-pyre.mid"
 	#f = "midis/native faith.mid"
 	#f = "midis/portal_still_alive.mid"
@@ -79,13 +83,30 @@ if __name__ == "__main__":
 	#f = "midis/Windmill 2.mid"
 	#f = "midis/Windmill.mid"
 	
+	#holy hell
+	#s = True
+	#f = "midis/black midi/Death Waltz.mid"
+	#f = "midis/black midi/bad apple 4.6 million.mid"#memoryerror
+	#f = "midis/black midi/The Titan_2.mid"
+	#f = "midis/black midi/.mid"
 	
+	gen = audio.generator()
+	#gen.instruments = [audio.sineWave]*16
+	gen.instruments = [audio.squareWave]*16
+	#gen.instruments = [audio.triangleWave]*16
+	#gen.instruments = [audio.sawtoothWave]*16
+	#gen.instruments = [audio.dafuqWave]*16
 	
-	#main(audio.squareGenerator,    keyboard=k, midifile=f)
-	#main(audio.sineGenerator,      keyboard=k, midifile=f)
-	#main(audio.triangleGenerator,  keyboard=k, midifile=f)
-	#main(audio.sawtoothGenerator,  keyboard=k, midifile=f)
-	main(audio.dafuqGenerator,     keyboard=k, midifile=f)
+	for i in xrange(16):
+		pass
+		gen.instruments[i] = audio.AddAttack2Wave(gen.instruments[i], 0.5)
+		#gen.instruments[i] = audio.AddCrush2Wave(gen.instruments[i], 4)
+	
+	gen.instruments[9] = audio.highhatBeat
+	#gen.instruments[9] = audio.snareBeat
+	#gen.instruments[9] = audio.drumBeat
+	
+	main(gen, keyboard=k, midifile=f, silent=s)
 	
 
 
